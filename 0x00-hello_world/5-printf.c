26b32aacffc086f8fc6f41524e92aaeb3a379ada
ngari-kev <kevngariwangui@gmail.com>
2023-08-15 23:24:02 +0300

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*main - The function prints to standard output
+*Return: 0 (Succes)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
