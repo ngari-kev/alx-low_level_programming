59b6c1fe0581e289d859e91a7c586116aa4ba329
ngari-kev <kevngariwangui@gmail.com>
2023-08-17 02:57:08 +0300

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - prints out numbers from 0 to 9
+*then a new line
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
