b714ad2c349227cb6925da52ea4d30e4b5444cd7
ngari-kev <kevngariwangui@gmail.com>
2023-08-17 03:16:34 +0300

    separate

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Numbers must be separated by ,, followed by a space
+*then a new line
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(", ")
+	}
+	putchar('\n');
+	return (0);
+}
