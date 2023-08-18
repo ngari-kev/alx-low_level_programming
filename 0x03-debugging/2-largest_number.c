1a8f36a0b47b63fccef294c98a27ada62a3b1303
ngari-kev <kevngariwangui@gmail.com>
2023-08-19 00:23:04 +0300

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
-	{
+	if (a > b && c > b)
 		largest = a;
-	}
 	else if (b > a && a > c)
-	{
 		largest = b;
-	}
 	else
-	{
 		largest = c;
-	}
 	return (largest);
 }
