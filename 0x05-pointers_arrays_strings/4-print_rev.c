bc1eae0d1bb86ebf3e6739398df692353a71d94d
ngari-kev <kevngariwangui@gmail.com>
2023-08-21 23:28:08 +0300

    print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
 *print_rev - prints in reverse to stdout
-*@s: reference/address 
-@x: index
+*@s: reference/address
 */
 void print_rev(char *s)
 {
 	int x = 0;
-	
+
 	while (s[x] != '\0')
 	{
 		x++;
