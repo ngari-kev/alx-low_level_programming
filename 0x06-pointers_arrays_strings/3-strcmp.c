13051f1f43c1cea2f413999072347627539e9a23
ngari-kev <kevngariwangui@gmail.com>
2023-08-26 14:59:16 +0300

    string comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,37 @@
+/**
+*_strcmp - compares two strings
+*@s1: string 1
+*@s2: string 2
+*
+*Return: 0 if equal, -15 if s1 is greater
+*and 15 if s2 is s2 is greater
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, t = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			t = s1[i] - s2[i];
+			if (t > 0)
+				t = 15;
+			else if (t < 0)
+				t = -15;
+			else
+				t = 0;
+			return (t);
+		}
+		i++;
+	}
+	/* Compare the lengths of the strings*/
+	t = s1[i] - s2[i];
+	if (t > 0)
+		t = 15;
+	else if (t < 0)
+		t = -15;
+	else
+		t = 0;
+	return (t);
+}
