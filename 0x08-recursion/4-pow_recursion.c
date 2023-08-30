f7f717449def8e14a264027a260c0824c96c95dc
ngari-kev <kevngariwangui@gmail.com>
2023-08-31 01:28:39 +0300

    raising to what power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,15 @@
+/**
+*_pow_recursion - raises x to the power of y
+*@x: number to be raised to the power of y
+*@y: number of times x will be multiplied by itself
+*Return: x to the power of y
+*/
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
+	if (y == 0)
+		return (1);
 	if (y == 1)
 		return (x);
 	return (x * _pow_recursion(x, y - 1));
