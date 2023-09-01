3f06c43ebbb700af8e27ad8e45bc9b1c77559e9f
ngari-kev <kevngariwangui@gmail.com>
2023-09-02 02:04:04 +0300

    name of the function

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,7 @@
-#include "main.h"
+#include <stdio.h>
 int main(int argc, char *argv[])
 {
-	if (argc >= 0)
-	{
-		_putchar(n);
-	}
-	return (0);
+        if (argc)
+		printf("%s\n", argv[0]);
+        return (0);
 }
