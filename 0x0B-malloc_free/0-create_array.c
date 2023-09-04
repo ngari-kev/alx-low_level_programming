364a4cd51dff6f53efac803d1da8a521d7eb0c84
ngari-kev <kevngariwangui@gmail.com>
2023-09-04 14:13:30 -0700

    Adding the malloc return value checker

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,11 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	if (a == NULL)
-		return (NULL);
 	else
 	{
 		a = malloc(sizeof(char) * size);
+		if (a == NULL)
+			return (NULL);
 		for (i = 0; i < size; i++)
 		{
 			a[i] = c;
