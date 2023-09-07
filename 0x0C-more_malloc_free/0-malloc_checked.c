9969e516777833a4474dfdc3ffcef95e10b3e26b
ngari-kev <kevngariwangui@gmail.com>
2023-09-07 17:23:21 +0300

    Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 *malloc_checked - allocates memory space and checks if it was successful
 *@b: number of bytes to be created
 *
-*
+*Return: 98 if error and pointer if success
 */
 void *malloc_checked(unsigned int b)
 {
