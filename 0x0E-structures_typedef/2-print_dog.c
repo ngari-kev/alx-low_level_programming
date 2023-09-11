b567db68281e47f70e65976b314e67dc0c6711ca
ngari-kev <kevngariwangui@gmail.com>
2023-09-11 15:22:04 -0700

    printing details of a structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,7 +6,26 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+	{
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+	if (d->age == 0)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: (nil)\n");
+		printf("Owner: %s\n", d->owner);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: (nil)\n");
+	}
+
 }
