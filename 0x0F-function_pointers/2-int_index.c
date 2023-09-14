25103c116c936e3a2140e286246d198522cd72e6
ngari-kev <kevngariwangui@gmail.com>
2023-09-14 17:06:44 +0300

    in the case where null is encountered

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,15 +10,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int a = 0, i = 0;
 
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		a = cmp(array[i]);
-		if (a == 1)
-			return (i);
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			a = cmp(array[i]);
+			if (a == 1)
+				return (i);
+		}
+		if (i == size)
+			return (-1);
 	}
-	if (i == size)
-		return (-1);
 	return (0);
 }
