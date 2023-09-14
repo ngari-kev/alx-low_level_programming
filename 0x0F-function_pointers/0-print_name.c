e825908cc273c08081d7a13efe594157fe57b3b1
ngari-kev <kevngariwangui@gmail.com>
2023-09-14 15:23:12 +0300

    Using pointers to functions

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+  *print_name - prints name as per the name entered and the function passed
+  *@name: pointer to name
+  *@f: pointer to a function
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef MAIN
+#define MAIN
+#include<stdlib.h>
+void print_name(char *name, void (*f)(char *));
+
+
+
+#endif
