8dd5dbcb3196f3d251abcf2f1516c8ec9172e7bb
ngari-kev <kevngariwangui@gmail.com>
2023-09-15 23:46:45 +0300

    calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,28 +1,32 @@
 #include "3-calc.h"
-
+/**
+  *main - calls necessary function to run specified operator
+  *@argc: number of arguments
+  *@argv: array of arguments
+  *Return: 0 (Success)
+  */
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	int n1, n2, result, (*f)(int,int);
+	char *operator;
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[3]);
-    char *operator = argv[2];
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	operator = argv[2];
+	f = get_op_func(operator);
 
-    int (*func)(int, int) = get_op_func(operator);
-
-    if (func == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
-
-    int result = func(num1, num2);
-    printf("%d\n", result);
-
-    return (0);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = f(n1, n2);
+	printf("%d\n", result);
+	return (0);
 }
 
