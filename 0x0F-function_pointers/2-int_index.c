86da9ebf4003fb709dd20b6c0591bacf2f181ca1
ngari-kev <kevngariwangui@gmail.com>
2023-09-16 07:03:11 +0300

    betty compliance

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,9 +8,11 @@
   */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
 	if (array == NULL || cmp == NULL || size <= 0)
 		return -1;
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return i;
