7de066c418cac92a67a9918b77d3ea538c23e673
ngari-kev <kevngariwangui@gmail.com>
2023-09-16 06:48:50 +0300

    printing numbers separated by string

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - Prints integers separated by a given separator.
+ *
+ * This function accepts a variable number of integer arguments and prints them
+ * to the standard output, separated by the specified separator string.
+ * After printing all the numbers, a newline character is added to the output.
+ *
+ * @separator: The string to be printed between numbers (use NULL to omit).
+ * @n: The number of integers to be printed.
+ * @...: The list of integers to be printed.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int num = 0;
+	unsigned int i = 0;
+
+	va_list ptr;
+	va_start (ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ptr, int);
+		printf("%d", num);
+		if (separator != NULL && i < n-1)
+			printf("%s", separator);
+	}
+	va_end(ptr);
+	printf("\n");
+}
