f82028a441b0e38349291cb95c44665aa5b88af6
ngari-kev <kevngariwangui@gmail.com>
2023-09-16 07:00:21 +0300

    update on an error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,20 +8,12 @@
   */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int a = 0, i = 0;
-
-	if (array != NULL && cmp != NULL)
+	if (array == NULL || cmp == NULL || size <= 0)
+		return -1;
+	for (int i = 0; i < size; i++)
 	{
-		if (size <= 0)
-			return (-1);
-		for (i = 0; i < size; i++)
-		{
-			a = cmp(array[i]);
-			if (a == 1)
-				return (i);
-		}
-		if (i == size)
-			return (-1);
+		if (cmp(array[i]) != 0)
+			return i;
 	}
-	return (0);
+	return -1;/* No matching element found.*/
 }
