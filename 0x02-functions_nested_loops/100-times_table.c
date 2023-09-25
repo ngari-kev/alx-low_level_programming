7430219fa2dfc87e9d1cae731adc2afbb51fb212
ngari-kev <kevngariwangui@gmail.com>
2023-09-25 17:46:17 +0300

    advanced times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the 0 to n times table so long as 15<n>0
+ *@n: limit for the times table
+ */
+void print_times_table(int n)
+{
+	int i, j, k = 0;
+
+	if (n > 15 || n < 0)
+	{
+		printf("\n");
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (j == 0)
+				{
+					printf("%d", k);
+				}
+				else if (k > 10)
+				{
+					printf("%4d", k);
+				}
+				else
+				{
+					printf("%4d", k);
+				}
+				if (j < n)
+				{
+					printf(",");
+				}
+			}
+			printf("\n");
+		}
+	}
+}
