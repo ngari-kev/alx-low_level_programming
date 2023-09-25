4db81c9a31c9518f403abd49bb7f9680803e2248
ngari-kev <kevngariwangui@gmail.com>
2023-09-25 20:08:39 +0300

    fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+  *fib - prints fibonnaci numbers to a specified value
+  *@n: specified value
+  */
+void fib(int n)
+{
+	int a = 1, b = 2, c;
+
+	printf("%d", a);
+	while (b <= n)
+	{
+		printf(", %d", b);
+		c = a + b;
+		a = b;
+		b = c;
+		if (b > n)
+		{
+			printf("\n");
+		}
+	}
+}
+
+/**
+  *main - prints fibonacci series to a specified limit
+  *Return: 0
+  */
+int main(void)
+{
+    fib(50);
+
+    return (0);
+}
