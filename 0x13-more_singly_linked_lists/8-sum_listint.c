9c2d9289858e0cf6a6be60698d27d296948f3183
ngari-kev <kevngariwangui@gmail.com>
2023-09-27 00:01:26 +0300

    sum of data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  *sum_listint - sums the data held in every node
+  *@h: pointer to head node
+  *Return: sum or 0 if h is null
+  */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *h;
+
+	h = head;
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		sum += h->n;
+		h = h->next;
+	}
+	return (sum);
+}
