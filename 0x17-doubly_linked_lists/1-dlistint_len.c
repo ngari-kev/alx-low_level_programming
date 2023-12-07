3c17a11dea4a29a6d8002d7f9ba646f20e6e7014
ngari-kev <kevngariwangui@gmail.com>
2023-12-07 22:53:56 +0300

    No. of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+*dlistint_len - return the number of elements in the list
+*@h: pointer to the Head node of the list
+*Return: number of elements in the list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
