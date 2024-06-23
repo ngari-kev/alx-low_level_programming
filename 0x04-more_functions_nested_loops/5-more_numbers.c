5e1b106631fb950c7d47a25320082e22536e5c10
ngari-kev <kevngariwangui@gmail.com>
2024-06-23 22:03:13 +0300

    0-isuper

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *more_numbers - print 0-9 followed by a new line 10x
+ *more_numbers - print 0-14 followed by a new line 10x
  *
  *
  */
@@ -9,7 +9,7 @@ void more_numbers(void)
 	int i = 0, j = 0;
 	char str[20] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 1, 1, 2, 1, 3, 1, 4};
 
-	while (i <= 11)
+	while (i <= 9)
 	{
 		j = 0;
 		while (j <= 19)
